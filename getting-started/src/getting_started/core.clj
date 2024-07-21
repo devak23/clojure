e70bc626638c96d504a3c7893bc2332a27bafe28
Abhay Kulkarni <developer.ak23@gmail.com>
2024-07-21 15:57:15 +0530

    added some comments.

--- getting-started/src/getting_started/core.clj
+++ getting-started/src/getting_started/core.clj
@@ -8,28 +8,46 @@
 (defn -main
   "The main program"
   [& args]
+  ; typical hello world
   (println "I'm little teapot!")
+
+  ; Invoking a function which is defined above
   (println (hello "Abhay"))
+
+  ; Invoking the same function without any parameters
   (println (hello))
+
+  ; the syntax is to first specify the operator or function and then pass a list of arguments.
   (println (first [1 3 5 7 9]))
+
   (do (println "no prompt here" (+ 1 3)))
   (println "args passed were: " args)
   (println (.concat "It's Panda!! He is a Panda! " "What are you going to do big guy? Sit on me? Heh heh.."))
   (println (str " ..Heh heh" "Don't tempt me " "Now, I am going to use this..." "You want it? Come and get it"))
 
+  ; This is a classic case of if-else loop. The true will evaluate to the first line and false will evaluate to second
+  ; line.
   (println (if false
              "You have a become a problem that I have to deal with."
              "Contrary to popular belief, you are not the center of my universe. Hit him."))
 
+  ; if you want to execute a bulk of statements, you need to use the do syntax combined with if like so
   (if true
     (do (println "GOD!! What did you do to me???")
         (println "What did we do FOR you. It's lithium dioxide. It's not a cure. It's just gonna take the edge off.")))
 
+  ; evaluating booleans
   (println (= 1 1))
+
+  ; comparing the strings
   (println (= "Gandalf The grey" "Gandalf The white"))
 
+  ; Declaration of variables. Here we are assigning a list of superheroes for a variable. It is important to note
+  ; that variable name can have a hyphen. This is quite idiomatic and the compiler will change the hyphens to underscore
   (def my-fav-superheroes ["IronMan" "Wanda" "Thor"])
 
+  ; Here we are trying to use a join function of closure.string and that function is passed a list of parameters
+  ; which is provided by the map function.
   (println (clojure.string/join "," (map str my-fav-superheroes)))
   )
 
